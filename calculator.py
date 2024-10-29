f755e9ecb2351887468b1c234c2fb0a4ec642eca
CarlosDaniel-DSS <cdss9@aluno.ifal.edu.br>
2024-10-29 11:24:47 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 # pegar valores da conta
-x = int(input("digite o primeio número"))
-y = int(input("digite o segundo númro"))
+x = float(input("digite o primeio número:")) 
+y = float(input("digite o segundo número:"))
+z = float(input("digite o terceiro número"))
 # Calcula e exibe o resultado
-print(f"A soma de {x} + {y} é {x + y}")
+resultado = (x + y) * z # expressão que será calculada
+resultado = f"{resultado:_.2f}" # definindo o número de casas decimais e o separador de milhar por "_"
+resultado = resultado.replace(".",",").replace("_",".") # mudando os separadores do modelo americano para o brasileiro
+print(f"O quadrado da soma de {x} mais {y} elevado à {z} é {resultado}") 
