ffc8d3c9506450a193e2508c2315d22a2a7f984e
CarlosDaniel-DSS <cdss9@aluno.ifal.edu.br>
2024-11-26 12:06:25 -0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+# loop com fim:
+x = 1
+
+while x <= 4:
+    print (x, "Atumalaka")
+    x += 1
+
+# fórmula do loop ininito: [while true: print("blá, blá, blá")]
+
+# forma com lista:
+for x in range(4):
+    print (x, "Atumalaka")
