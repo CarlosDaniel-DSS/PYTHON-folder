dd00f5f60ef8a0ce25c376e50f4d18e609e83450
CarlosDaniel-DSS <cdss9@aluno.ifal.edu.br>
2026-06-19 15:03:33 -0300

    Refactor decryption process and input key handling
    Refactor decryption logic and input handling.

--- decifrar_c_arquivo.py
+++ decifrar_c_arquivo.py
@@ -1,52 +1,6 @@
-'''
-palavra = ""
-palavra_descriptografada = ""
-lista_palavras = []
-valor_ascii = 0
-ascii_A = ord('A')
-ascii_a = ord('a')
-
-
-arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "r+")
-
-for linha in arquivo:
-    for letra in linha:
-        mensagem += letra
-        try:
-            chave = int(input("Informe o valor da cifra de césar (deslocamento): "))
-            while chave > 26:
-                chave = chave % 26
-
-            for letra in palavra:
-                # Conversão da letra para ASCII
-                valor_ascii = ord(letra)
-
-                # Mantém letras maiúsculas
-                if letra >= 'A' and letra <= 'Z':
-                    nova_letra = ((valor_ascii - ascii_A - chave) % 26) + ascii_A
-                    palavra_descriptografada += chr(nova_letra)
-
-                # Mantém letras minúsculas
-                elif letra >= 'a' and letra <= 'z':
-                    nova_letra = ((valor_ascii - ascii_a - chave) % 26) + ascii_a
-                    palavra_descriptografada += chr(nova_letra)
-
-                # Mantém caracteres especiais
-                else:
-                    palavra_descriptografada += letra
-        except ValueError as err:
-            print("Valor inválido")
-            print(err)
-            print("------------------------------------------------")
-        else:
-            lista_palavras.append(palavra_descriptografada)
-            palavra_descriptografada = ""
-            cnt += 1
-
-#corrigir
-'''
-
 chave = int(input("Digite a chave pa1ra descriptografar a mensagem"))
+while chave > 26:
+    chave = chave % 26
 
 arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "r")
 criptografada = arquivo.read()
