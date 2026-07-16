be63740fa6e56f5b114eb52d4de2ce76768ade34
CarlosDaniel-DSS <cdss9@aluno.ifal.edu.br>
2026-07-16 08:56:42 -0300

    Refactor criptografia_de_mensagens function
    Refactor message encryption function to improve structure and add character handling.

--- cifrar_c_arquivo.py
+++ cifrar_c_arquivo.py
@@ -1,62 +1,139 @@
-palavra = ""
-palavra_criptografada = ""
-lista_palavras = []
-lista_criptografadas = []
-valor_ascii = 0
-ascii_A = ord('A')
-ascii_a = ord('a')
-cnt = 0
-
-while palavra != "s":
-    palavra = input("Digite uma palavra para ser criptografada (ou 's' para sair): ")
+'''def criptografia_de_mensagens():
     
-    if palavra != "s" and palavra != "":
-        try:
-            chave = int(input("Informe o valor da cifra de césamo(deslocamento): "))
-            while chave > 26:
-                chave = chave % 26
-            
-            for letra in palavra:
-                # Mantém letras maiúsuclas
-                if 'A' <= letra <= 'Z':
-                    nova_letra = chr((ord(letra) - ord('A') + chave) % 26 + ord('A'))
+    palavra = ""
+    palavra_criptografada = ""
+    lista_palavras = []
+    lista_criptografadas = []
+    valor_ascii = 0
+    ascii_A = ord('A')
+    ascii_a = ord('a')
+    cnt = 0
+    
+    #Pedindo a chave da criptografia de todas as palvras
+    try:
+        chave = int(input("Informe o valor da cifra de césamo(deslocamento) das palavras: "))
+        while chave > 26:
+            chave = chave % 26
+    except ValueError as err:
+                print("Valor inválido")
+                print(err)
+                print("------------------------------------------------")
+    
+    #Criptografando todas as palavras que o usuário digitar    
+    while palavra != "s":
+        palavra = input("Digite uma palavra para ser criptografada (ou 's' para sair): ")
+        
+        if palavra != "s" and palavra != "":
+                
+                for letra in palavra:
+                    # Mantém letras maiúsuclas
+                    if 'A' <= letra <= 'Z':
+                        nova_letra = chr((ord(letra) - ord('A') + chave) % 26 + ord('A'))
 
-                # Mantém letras minúsculas
-                elif 'a' <= letra <= 'z':
-                    nova_letra = chr((ord(letra) - ord('a') + chave) % 26 + ord('a'))
-                    
-                # Mantém caracteres especiais e números
-                else:
-                    nova_letra = letra
+                    # Mantém letras minúsculas
+                    elif 'a' <= letra <= 'z':
+                        nova_letra = chr((ord(letra) - ord('a') + chave) % 26 + ord('a'))
+                        
+                    # Mantém caracteres especiais e números
+                    else:
+                        nova_letra = letra
 
-                palavra_criptografada += nova_letra
-
-        except ValueError as err:
-            print("Valor inválido")
-            print(err)
-            print("------------------------------------------------")
+                    palavra_criptografada += nova_letra
+        
+                lista_palavras.append(palavra)
+                lista_criptografadas.append(palavra_criptografada)
+                palavra_criptografada = ""
+                
+                cnt += 1
         else:
-            lista_palavras.append(palavra)
-            lista_criptografadas.append(palavra_criptografada)
-            palavra_criptografada = ""
-            
-            cnt += 1
+            if palavra == "":
+                print("Nenhuma palavra foi digitada.")
     else:
-        if palavra == "":
-            print("Nenhuma palavra foi digitada.")
-else:
-    if cnt > 0:
-        print(f"\nOriginal: {lista_palavras}")
+        if cnt > 0:
+            print(f"\nOriginal: {lista_palavras}")
+            
+            #Colocando a criptografia no arquivo
+            arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "w")
+            arquivo.close()
+            
+            arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "r+")
+            for linha in arquivo:
+                pass
+            
+            for palavra in lista_criptografadas:
+                arquivo.write(f"\n{palavra}")
+            
+            arquivo.close()
+criptografia_de_mensagens()'''
+
+def criptografia_de_mensagens():
+    
+    palavra = ""
+    palavra_criptografada = ""
+    lista_palavras = []
+    lista_criptografadas = []
+    valor_ascii = 0
+    ascii_A = ord('A')
+    ascii_a = ord('a')
+    cnt = 0
+    
+    #Pedindo a chave da criptografia de todas as palvras
+    try:
+        chave = int(input("Informe o valor da cifra de césamo(deslocamento) das palavras: "))
+        while chave > 26:
+            chave = chave % 26
+    except ValueError as err:
+                print("Valor inválido")
+                print(err)
+                print("------------------------------------------------")
+    
+    #Criptografando todas as palavras que o usuário digitar    
+    while palavra != "s":
+        palavra = input("Digite uma palavra para ser criptografada (ou 's' para sair): ")
         
-        #Colocando a criptografia no arquivo
-        arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "w")
-        arquivo.close()
+        if palavra != "s" and palavra != "":
+                
+                for letra in palavra:
+                    # Mantém letras maiúsuclas
+                    if 'A' <= letra <= 'Z':
+                        nova_letra = chr((ord(letra) - ord('A') + chave) % 26 + ord('A'))
+                        inverter = ord(nova_letra)- 60
+                        nova_letra = chr(90 - inverter)
+
+                    # Mantém letras minúsculas
+                    elif 'a' <= letra <= 'z':
+                        nova_letra = chr((ord(letra) - ord('a') + chave) % 26 + ord('a'))
+                        inverter = ord(nova_letra)- 97
+                        nova_letra = chr(122 - inverter)
+                        
+                    # Mantém caracteres especiais e números
+                    else:
+                        nova_letra = letra
+
+                    palavra_criptografada += nova_letra
         
-        arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "r+")
-        for linha in arquivo:
-            pass
-        
-        for palavra in lista_criptografadas:
-            arquivo.write(f"\n{palavra}")
-           
-        arquivo.close()
+                lista_palavras.append(palavra)
+                lista_criptografadas.append(palavra_criptografada)
+                palavra_criptografada = ""
+                
+                cnt += 1
+        else:
+            if palavra == "":
+                print("Nenhuma palavra foi digitada.")
+    else:
+        if cnt > 0:
+            print(f"\nOriginal: {lista_palavras}")
+            
+            #Colocando a criptografia no arquivo
+            arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "w")
+            arquivo.close()
+            
+            arquivo = open("/home/carlosdaniel/PYTHON-folder-main/terceiro-ano/arquivos/mensagem.txt", "r+")
+            for linha in arquivo:
+                pass
+            
+            for palavra in lista_criptografadas:
+                arquivo.write(f"\n{palavra}")
+            
+            arquivo.close()
+criptografia_de_mensagens()
